0453716a93dec17886cafed84a688ee6f502cb18
Daniel A. A. Pelsmaeker <647530+Virtlink@users.noreply.github.com>
2021-02-18 01:20:50 +0100

    Update to Intellij 2020.3

--- example/tiger/statix/tiger.statix.intellij/build.gradle.kts
+++ example/tiger/statix/tiger.statix.intellij/build.gradle.kts
@@ -18,13 +18,13 @@ languageIntellijProject {
 }
 
 intellij {
-  version = "2019.3.2"
+  version = "2020.3"
 }
 
 // Use Java 8 version of JBR (JetBrains Runtime) to run the IDE, to prevent illegal reflective operation errors.
-tasks.getByName<org.jetbrains.intellij.tasks.RunIdeTask>("runIde") {
-  this.jbrVersion("8u232b1638.6")
-}
+//tasks.getByName<org.jetbrains.intellij.tasks.RunIdeTask>("runIde") {
+//  this.jbrVersion("8u232b1638.6")
+//}
 
 // Skip non-incremental, slow, and unnecessary buildSearchableOptions task from IntelliJ.
 tasks.getByName("buildSearchableOptions").onlyIf { false }
